086bba863c73945de59fb420773d55c5a7825316
sm <smirnovceo@yandex.ru>
2022-04-08 16:33:57 +0500

    add tests + refactor CI action

---
+++ components/user_service/user_backend/tests/unit/conftest.py
@@ -0,0 +1,55 @@
+from classic.messaging import Message, Publisher
+
+from user.application import dataclasses, services
+
+from unittest.mock import Mock
+
+import pytest
+
+from user.application import interfaces
+
+
+@pytest.fixture(scope="function")
+def user_repo(user_1, user_2):
+    user_repo = Mock(interfaces.UserRepoI)
+    user_repo.get_by_id = Mock(return_value=user_1)
+    user_repo.get_all = Mock(return_value=[user_1, user_2])
+    user_repo.create = Mock(return_value=user_1)
+    user_repo.delete = Mock(return_value=user_1)
+    return user_repo
+
+
+@pytest.fixture(scope="function")
+def publisher():
+    publisher = Mock(Publisher)
+    return publisher
+
+
+@pytest.fixture(scope="function")
+def user_1():
+    return dataclasses.User(
+        name="test_name",
+        surname="test_surname",
+        email="test_email@mail.ru",
+        status="test_status",
+        id=1
+    )
+
+
+@pytest.fixture(scope="function")
+def user_2():
+    return dataclasses.User(
+        name="test_name",
+        surname="test_surname",
+        email="test_email@mail.ru",
+        status="test_status",
+        id=2
+    )
+
+
+@pytest.fixture(scope="function")
+def user_info_1():
+    return services.UserInfo(
+        name="test_name_upd",
+        surname="test_surname_upd"
+    )
---
+++ components/user_service/user_backend/tests/unit/test_service.py
@@ -0,0 +1,56 @@
+import pytest
+
+from user.application.services import UserService
+from user.application import errors
+from unittest.mock import Mock
+
+
+@pytest.fixture(scope="function")
+def service(user_repo, publisher):
+    return UserService(user_repo=user_repo, publisher=publisher)
+
+
+def test__get_by_id(service, user_repo, user_1):
+    assert service.get_by_id(1) == user_1
+
+
+def test__get_by_by_exception(service, user_repo):
+    id_ = 1
+    user_repo.get_by_id = Mock(return_value=None)
+    with pytest.raises(errors.NoUser):
+        service.get_by_id(id_)
+
+
+def test__get_all(service, user_repo, user_1, user_2):
+    assert service.get_all() == [user_1, user_2]
+
+
+def test__get_all_exception(service, user_repo):
+    user_repo.get_all = Mock(return_value=None)
+    with pytest.raises(errors.NoUsers):
+        service.get_all()
+
+
+def test__create(service, user_repo, user_1):
+    test_case = {
+        "name": "test_name",
+        "surname": "test_surname",
+        "email": "test_email@mail.ru",
+        "status": "test_status"
+    }
+    assert service.create(**test_case) == user_1
+
+
+def test__update(service, user_repo, user_1):
+    test_case = {
+        "name": "test_name_upd",
+        "surname": "test_surname_upd"
+    }
+    service.get_by_id = Mock(return_value=user_1)
+    assert service.update(**test_case) == user_1
+
+
+def test_delete(service, user_repo, user_1):
+    id_ = 1
+    service.get_by_id = Mock(return_value=user_1)
+    assert service.delete(id_) == user_1
